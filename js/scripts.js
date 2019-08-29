2bf0809c5ba99dbca9ca8a8b4c7375655a107cae
coolshadow205 <jay.ashra251198@gmail.com>
2019-08-29 16:50:54 +0530

    Modified css/style.css & js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $('#todoList ul').sortable({
-        items: "li:not('.listTitle, .addItem')",
-        connectWith: "ul",
+        items: "li:not('.listTitle, .addItem')",// which items must be selected for sorting.
+        connectWith: "ul",//connectWith allows us to sort the task in other days as will because we have specified 'ul' parent of all li
         dropOnEmpty: true,
-        placeholder: "emptySpace",
+        placeholder: "emptySpace",//placeholder allows us to use change the ui of placeholder and by writing .placeholder in css
     });
-    $('input').keydown(function(src){
-        if(src.keyCode == 13 && $(this).val()!='')
-        {
-            //ENTER KEY WAS PRESSED
-            var item = $(this).val();
-            
-            $(this).parent().parent().append('<li>' + item + '</li>');
-            $(this).val('');
-        }
-    });
-    $('#trash').droppable({
-        drop: function(event, ui){
-            ui.draggable.remove();
-        }
-    });
+$('input').keydown(function(src){
+    if(src.keyCode == 13 && $(this).val()!='')
+    {
+        //ENTER KEY WAS PRESSED
+        var item = $(this).val();
+        
+        $(this).parent().parent().append('<li>' + item + '</li>');
+        $(this).val('');
+    }
+});
+$('#trash').droppable({
+    drop: function(event, ui){
+        ui.draggable.remove();
+    }
+});
